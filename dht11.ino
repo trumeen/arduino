aa23eb5c981933a0a44e67c1a4078afbb484b8cd
trumeen <ddj1@163.com>
2020-09-08 15:46:02 +0800

    Update dht11.ino

--- dht11.ino
+++ dht11.ino
@@ -10,9 +10,9 @@
 int pinDHT11 = 2;                  //IO2
 SimpleDHT11 dht11(pinDHT11);
 
-const char* ssid = "XXXXXX";//手机Wi-Fi热点名
-const char* password = "XXXXXX";//手机Wi-Fi热点密码
-const char* mqtt_server = "192.168.4.1";//服务器地址 可以是域名
+const char* ssid = "WeWork";//手机Wi-Fi热点名
+const char* password = "P@ssw0rd";//手机Wi-Fi热点密码
+const char* mqtt_server = "10.125.30.30";//服务器地址 可以是域名
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -20,6 +20,9 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 int ledPin = 2; // GPIO2 of ESP8266
+
+int lastTemperature = 0;
+int lastHumidity = 0;
 
 void setup_wifi() {
   delay(10);
@@ -37,29 +40,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-  if ((char)payload[0] == '1') {          // 通过接收的信息 '1' 来控制开或关
-    digitalWrite(ledPin, HIGH); 
-      Serial.println("HIGH");
-  } else {
-    digitalWrite(ledPin, LOW); 
-    Serial.println("LOW");
-  }
 }
 
 void reconnect() {
     String clientId = "ESP-01A-";
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str())) {         //连接成功
-      client.subscribe("开关控制");
-      client.publish("开关控制", "0");     // 连接成功发送主题和消息
+      Serial.println("MQTT连接成功！！！");
+      // client.subscribe("/trumeen/cmd");
+      client.publish("/trumeen/home/temperature/state", "ONLINE");     // 连接成功发送主题和消息
     } else {
       delay(5000);                                  // Wait 5 seconds before retrying
     }
 }
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);  
+  pinMode(ledPin, INPUT);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -74,9 +70,36 @@ void readTempAndHumidity(){
     Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
     return;
   }
+  //  String tmp = "{";            //字符串拼接
+  //  tmp += String('"');
+  //  tmp += String("temperature");
+  //  tmp += String('"');
+  //  tmp += String(':');
+  //  tmp += String(temperature);
+  //  tmp += String(',');
+  //  tmp += String('"');
+  //  tmp += String("humidity");
+  //  tmp += String('"');
+  //  tmp += String(":");
+  //  tmp += String(humidity);
+  //  tmp += String("}");
+  // client.publish("/trumeen/publish", tmp.c_str());     // 连接成功发送主题和消息
   
   Serial.print((int)temperature); Serial.print(" *C, "); 
   Serial.print((int)humidity); Serial.println(" H");
+   if((int)temperature!=lastTemperature){
+      String temp = "";
+      temp+=(int)temperature;
+      client.publish("/trumeen/home/temperature/value", temp.c_str());     // 连接成功发送主题和消息
+     lastTemperature = (int)temperature;
+   }
+  
+  if((int)humidity!=lastHumidity){
+      String temp = "";
+      temp+=(int)humidity;
+      client.publish("/trumeen/home/humidity/value", temp.c_str());     // 连接成功发送主题和消息
+      lastHumidity = (int)humidity;
+  }
   delay(1500);
 }
 
@@ -85,5 +108,5 @@ void loop() {
     reconnect();
   }
   client.loop();
-  readTempAndHumidity()
+  readTempAndHumidity();
 }
